3cc734af2fb6dea0569e9672d404d25713a7c0f8
Emilia Yoffie <emiliayoffie@gmail.com>
2022-05-02 23:21:06 -0400

    worked on query file

--- emiliaReadMe.md
+++ emiliaReadMe.md
@@ -1,12 +1,13 @@
 File Description:
-    Create query methods that are offered either by the deno mongo driver or by mongoose. 
+    Create query methods that are offered either by the deno mongo driver or by mongoose. Have we addedd custom methods? 
 
 
-- class Query - . 
+- class Query - Should we write any info about this class? . 
 
 - Interfaces: 
-    -MatchInterface - . 
-    -GroupInterface - . 
+    -MatchInterface - an interface which desribes the shape of the match operator type. 
+
+    -GroupInterface - an interface which describes the shape of the group operator type. 
 
 - Methods:
     findOne, 
@@ -37,10 +38,17 @@ File Description:
     
     deleteOne() - Deletes the first document that matches conditions from the collection. It returns an object with the property deletedCount indicating how many documents were deleted. Behaves like findOneAndRemove(), but deletes at most one document regardless of the single option. 
 
-    findByIdAndDelete() - Issue a MongoDB findOneAndDelete() command by a document's _id field. In other words, findByIdAndDelete(id) is a shorthand for findOneAndDelete({ _id: id }). This function triggers the following middleware.
+    findByIdAndDelete() - Issue a MongoDB findOneAndDelete() command by a document's _id field. In other words, findByIdAndDelete(id) is a shorthand for findOneAndDelete({ _id: id }). This function triggers the following middleware: findOneAndDelete(); 
 
     findOneAndDelete() - Issue a MongoDB findOneAndDelete() command. Finds a matching document, removes it, and passes the found document (if any) to the callback. Executes the query if callback is passed. This function triggers the following middleware. This function differs slightly from Model.findOneAndRemove() in that findOneAndRemove() becomes a MongoDB findAndModify() command, as opposed to a findOneAndDelete() command. For most mongoose use cases, this distinction is purely pedantic. You should use findOneAndDelete() unless you have a good reason not to.
 
     findOneAndRemove() - Issue a mongodb findAndModify remove command. Finds a matching document, removes it, passing the found document (if any) to the callback. Executes the query if callback is passed. 
 
     findByIdAndRemove() - Issue a mongodb findAndModify remove command by a document's _id field. findByIdAndRemove(id, ...) is equivalent to findOneAndRemove({ _id: id }, ...). Finds a matching document, removes it, passing the found document (if any) to the callback. Executes the query if callback is passed. This function triggers the following middleware: findOneAndRemove(). 
+
+
+    ToDo: 
+        -update error handling? 
+        -refactor how the connection is brought in? 
+        -does everything that needs options have options passed in? 
+        -add console log messages
--- lib/emiliaquery.ts
+++ lib/emiliaquery.ts
@@ -1,15 +1,16 @@
-/*Create a query class*/
+/**
+ * 
+ * @description This file defines the query class.
+ * 
+ */
 
-// import { Collection } from '../deps.ts';
 import { Connection } from './connections.ts';
 import { Bson } from "https://deno.land/x/mongo@v0.29.4/mod.ts";
-// import {FindAndModifyOptions } from 'https://deno.land/x/mongo@v0.29.4/src/types.ts'; 
-
+import { FindOptions } from 'https://deno.land/x/mongo@v0.29.4/src/types.ts'; 
 
 interface MatchInterface {
   $match: { [unknownKeyName: string]: string };
 }
-
 interface GroupInterface {
   $group: {
     [unknownKeyName: string]: string | { $sum: number };
@@ -18,7 +19,6 @@ interface GroupInterface {
 
 class Query {
   public collectionName: string;
-  // Refactor how connection is brought in
   public connection: Connection;
 
   constructor(collectionName: string) {
@@ -27,90 +27,122 @@ class Query {
       'mongodb+srv://wgreco13:g3HUuathwbVEisEj@cluster0.adcc3.mongodb.net/dangoDB?authMechanism=SCRAM-SHA-1'
     );
   }
-
-
-    /*Returns one document that satisfies the specified query criteria on the collection or view.  */
-    public async findOne(queryObject: Record<string, string>) {
+ /**
+     * Returns one document that satisfies the specified query criteria on the collection or view.
+     *
+     * @param query - The query used to match documents
+     * @param options Additional options for the operation (e.g. lean, populate, projection)
+     * @param callback 
+     * @returns The document matched and modified
+     * example: query.findOne({ username: 'newtest2' });
+     */
+    public async findOne(queryObject: Record<string, string>, options?: FindOptions, callback?: (input: unknown) => unknown) {
       try {
+
         const db = await this.connection.connect();
-
         const collection = db.collection(this.collectionName);
-        const data = await collection.findOne(queryObject);
-        console.log(data);
-
+        const data = await collection.findOne(queryObject, options);
+       
+        if(callback) return callback(data); 
         await this.connection.disconnect();
+        return data;
 
       } catch (error) {
         throw new Error(`Error in findOne function. ${error}`);
       }
     }
-
-
-    /*Selects documents in a collection or view and returns a cursor to the selected documents. */
-    public async find(allQueryObjects?: Record <string, unknown>) {
+ /**
+     * Returns one document that satisfies the specified query criteria on the collection or view.
+     *
+     * @param query - Selects documents in a collection or view and returns a cursor to the selected documents.
+     * @param options Additional options for the operation (e.g. lean, populate, projection)
+     * @param callback 
+     * @returns A count of the documents in the database. 
+     * example: query.find();
+     */
+    public async find(allQueryObjects?: Record <string, unknown>, options?: FindOptions, callback?: (input: unknown) => unknown) {
       try {
-        const db = await this.connection.connect();
 
+        const db = await this.connection.connect();
         const collection = db.collection(this.collectionName);
-        const data = await collection.find(allQueryObjects);
+        const data = await collection.find(allQueryObjects, options);
         const dataRes = await data.toArray();
 
-        console.log(dataRes);
-
+        if(callback) return callback(data); 
         await this.connection.disconnect();
+        return dataRes;
+
       } catch (error) {
         throw new Error(`Error in find function. ${error}`);
       }
     }
-
-    
-    /* Returns the count of documents that match the query for a collection or view. */
-    public async countDocuments(queryObject: Record<string, string>) {
+ /**
+    Counts number of documents matching filter in a database collection.
+     * @param Filter. 
+     * @param Additional options for the operation.
+     * @param Optional callback such as (err, count); 
+     * @returns a count (number);
+     * example: query.countDocuments({ username: 'test' });
+     */
+    public async countDocuments(queryObject: Record<string, string>, callback?: (input: unknown) => unknown) {
       try {
-        const db = await this.connection.connect();
 
+        const db = await this.connection.connect();
         const collection = db.collection(this.collectionName);
         const data = await collection.countDocuments(queryObject);
 
-        console.log(data);
-
+        if(callback) return callback(data); 
         await this.connection.disconnect();
+        return data; 
+        
       } catch (error) {
         throw new Error(`Error in countDocuments function. ${error}`);
       }
     }
-    /*estimatedDocumentCount() = Returns the count of all documents in a collection or view. The method wraps the count command. */
+    /**
+    Returns the count of all documents in a collection or view. The method wraps the count command.   
+     * @param Additional options for the operation.
+     * @param Optional callback such as (err, count); 
+     * @returns a count (number);
+     * example: query.estimatedDocumentCount(); 
+     */
     public async estimatedDocumentCount() {
       try {
-        const db = await this.connection.connect();
 
+        const db = await this.connection.connect();
         const collection = db.collection(this.collectionName);
         const data = await collection.estimatedDocumentCount();
-
-        console.log(data);
-
+       
         await this.connection.disconnect();
+        return data; 
+
       } catch (error) {
         throw new Error(`Error in estimatedDocumentCount function. ${error}`);
       }
     }
-    /*Aggregation operations process multiple documents and return computed results. You can use aggregation operations to:
+    /**
+        Aggregation operations process multiple documents and return computed results. You can use aggregation operations to:
         Group values from multiple documents together.
         Perform operations on the grouped data to return a single result.
-        Analyze data changes over time. */
-
-    //  public async aggregate(arg1: [{$match:{[unknownKeyName:string]: string}}]) {
+        Analyze data changes over time.      
+     * @param Aggregation pipeline as an array of objects.
+     * @returns Documents returned are plain javascript documents;
+     * example: query.aggregate([
+          { $match: { username: 'test' } },
+          { $group: { _id: '$username', total: { $sum: 1 } } },
+        ]);
+     */
     public async aggregate(arg1: [MatchInterface, GroupInterface]) {
       try {
-        const db = await this.connection.connect();
 
+        const db = await this.connection.connect();
         const collection = db.collection(this.collectionName);
         const data = await collection.aggregate(arg1);
         const dataRes = await data.toArray();
 
-        console.log(dataRes);
-
         await this.connection.disconnect();
+        return dataRes;
+
       } catch (error) {
         throw new Error(`Error in aggregate function. ${error}`);
       }
@@ -118,42 +150,24 @@ class Query {
    /**
      * Find and modify a document in one, returning the matching document.
      *
-     * @param query The query used to match documents
-     * @param options Additional options for the operation (e.g. containing update
-     * or remove parameters)
+     * @param The query used to match documents
+     * @param Additional options for the operation (e.g. sort, limit, skip)
      * @returns The document matched and modified
      * example: A.findByIdAndRemove(id, options, callback)
      */
 
   public async findAndModify(filter?: Record<string, unknown>, options?: Record<string, number | unknown>) {
     try {
-      const db = await this.connection.connect();
 
+      const db = await this.connection.connect();
       const collection = db.collection(this.collectionName);
       const data = await collection.findAndModify(filter, options);
 
-      console.log(data);
-
       await this.connection.disconnect();
+      return data; 
+
     } catch (error) {
       throw new Error(`Error in findandModify function. ${error}`);
-    }
-  }
-
-  public async deleteOne(queryObject: Record<string, unknown>) {
-    try {
-      const db = await this.connection.connect();
-
-      const collection = db.collection(this.collectionName);
-      const data = await collection.deleteOne(queryObject); // returns number of deleted documents
-      console.log(data);
-      const formattedReturnObj = { deletedCount: data };
-      console.log(formattedReturnObj);
-
-      await this.connection.disconnect();
-    } catch (error) {
-      //  need to update error handling
-      throw new Error(`Error in deleteOne function. ${error}`);
     }
   }
 
@@ -161,29 +175,24 @@ class Query {
     try {
     
       const stringId = new Bson.ObjectId(id)
-     
       const db = await this.connection.connect();
-
       const collection = db.collection(this.collectionName);
       const data = await collection.deleteOne({_id: stringId});
-      
-      console.log('successfully deleted: ', data);
+    
 
       await this.connection.disconnect();
+      return data; 
 
     } catch (error) {
       throw new Error(`Error in findByIdAndDelete function. ${error}`);
     }
   }
 
-  //does this need options???? or is remove the only option 
-
   public async findOneAndRemove(queryObject: Record<string, string>, callback?: (input: unknown) => unknown) {
     try {
-      const db = await this.connection.connect();
 
+      const db = await this.connection.connect();
       const collection = db.collection(this.collectionName);
-     
       const data = await collection.findAndModify(queryObject, {remove: true});
    
       if(callback) await callback(data);
@@ -201,11 +210,8 @@ class Query {
     try {
     
       const stringId = new Bson.ObjectId(id)
-     
       const db = await this.connection.connect();
-
       const collection = db.collection(this.collectionName);
-
       const data = await collection.findAndModify({_id: stringId}, { remove: true })
 
       if(callback) await callback(data); 
@@ -219,9 +225,7 @@ class Query {
   
 }
 
-
 const query = new Query('new');
-
 
 // query.findOne({ username: 'newtest2' });
 query.find()
