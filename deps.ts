92b180d319cbbbe5aa07912fc2681ec8934ed5fc
wgreco13 <wgreco13@gmail.com>
2022-05-13 12:00:02 -0400

    Complete datatypes and connections tests

--- deps.ts
+++ deps.ts
@@ -3,20 +3,56 @@
  * To add additional dependencies, add an export statement.
  */
 
- export {
-    MongoClient,
-    Database,
-    Collection,
-    Bson
+export {
+  MongoClient,
+  Database,
+  Collection,
+  Bson
 } from "https://deno.land/x/mongo@v0.29.4/mod.ts";
 
+export type {
+  CountOptions,
+  InsertOptions,
+  UpdateOptions,
+  FindAndModifyOptions,
+  DropOptions,
+  AggregateOptions,
+  FindOptions,
+  DeleteOptions,
+} from 'https://deno.land/x/mongo@v0.29.4/src/types.ts';
+
 export {
-    CountOptions,
-    InsertOptions,
-    UpdateOptions,
-    FindAndModifyOptions,
-    DropOptions,
-    AggregateOptions,
-    FindOptions,
-    DeleteOptions,
-} from 'https://deno.land/x/mongo@v0.29.4/src/types.ts';}
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from 'https://deno.land/std@0.138.0/testing/bdd.ts';
+
+export {
+  assert,
+  assertAlmostEquals,
+  assertArrayIncludes,
+  assertEquals,
+  assertExists,
+  assertFalse,
+  assertInstanceOf,
+  assertIsError,
+  assertMatch,
+  assertNotEquals,
+  assertNotMatch,
+  assertNotStrictEquals,
+  assertObjectMatch,
+  assertRejects,
+  assertStrictEquals,
+  assertStringIncludes,
+  assertThrows,
+  equal,
+  fail,
+  unimplemented,
+  unreachable,
+} from 'https://deno.land/std@0.138.0/testing/asserts.ts';
+
+import * as dotenv from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+export { dotenv }
